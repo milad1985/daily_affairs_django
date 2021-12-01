b12b27637f2f1cf4052bce5cb0b8718f5a63cec5
Milad Rezaei <m.rezaei@bankrefah.ir>
2021-12-01 13:27:33 +0330

    create requirments

--- web/models.py
+++ web/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AnonymousUser, User
 
 
 # Create your models here.
@@ -11,9 +11,15 @@ class Expense(models.Model):
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return "{} -- {}".format(self.name, self.amount)
+
 
 class Income(models.Model):
     name = models.CharField(max_length=255)
     amount = models.BigIntegerField()
     date = models.DateTimeField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{} -- {}".format(self.name, self.amount)
